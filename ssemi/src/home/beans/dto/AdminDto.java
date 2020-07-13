1124a4d678e7550e529048ec53b2fcefc48644d4
crysta49 <crysta49@naver.com>
2020-07-13 17:48:16 +0900

    관리자 로그인 기능 구현

--- ssemi/src/home/beans/dto/AdminDto.java
+++ ssemi/src/home/beans/dto/AdminDto.java
@@ -1,5 +1,8 @@
 package home.beans.dto;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class AdminDto {
 	
 	
@@ -10,6 +13,16 @@ public class AdminDto {
 	private String admin_login;
 	private String admin_auth ;
 	public AdminDto() {
+		
+	}
+	
+	public AdminDto(ResultSet rs) throws SQLException {
+		this.setAdmin_no(rs.getInt("admin_no"));
+		this.setAdmin_id(rs.getString("admin_id"));
+		this.setAdmin_pw(rs.getString("admin_pw"));
+		this.setAdmin_join(rs.getString("admin_join"));
+		this.setAdmin_login(rs.getString("admin_login"));
+		this.setAdmin_auth(rs.getString("admin_auth"));
 		
 	}
 	public int getAdmin_no() {
