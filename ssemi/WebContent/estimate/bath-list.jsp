39e9baaebd832bfd2966f1a7fc80eb3eea7688b7
user1 <user1@DESKTOP-QD9ND2F>
2020-07-15 11:12:55 +0900

    내꺼

---
+++ ssemi/WebContent/estimate/bath-list.jsp
@@ -0,0 +1,77 @@
+<%@page import="home.beans.dao.EstimateDao"%>
+<%@page import="home.beans.dao.MemberDao"%>
+<%@page import="home.beans.dto.MemberDto"%>
+<%@page import="java.util.List"%>
+<%@page import="home.beans.dto.EstimateDto"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    
+    <%
+	MemberDto mdto = (MemberDto)session.getAttribute("userinfo");
+	String bath_member = mdto.getMember_id();
+	
+	EstimateDao edao = new EstimateDao();
+	List<EstimateDto> list = edao.getList(bath_member);   %>
+	
+	
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+
+<style>
+
+
+
+</style>
+</head>
+<body>
+
+<table border="1" width="90%">
+		<thead>
+			<tr>
+				<th>번호</th>
+				<th width="40%">가격</th>
+				<th>작성자</th>
+				<th>작성일</th>
+			
+	
+			</tr>
+		</thead>
+		<tbody align="center">
+			<%-- list의 데이터를 하나하나 edto라는 이름으로 접근하여 출력 --%>
+			<%for(EstimateDto edto : list ){ %>
+			<tr>
+				<td><%=edto.getBath_no()%></td>
+				<td align="left">
+				
+					
+					<!-- 가격 -->
+					<a href="bath-content.jsp?bath_no=<%= edto.getBath_no()%>">
+						<%=edto.getBath_price()%>
+					</a>
+									
+				</td>
+				<td>
+						<%=edto.getBath_member()%>
+				</td>
+				<td><%=edto.getBath_autotime()%></td>
+			</tr>
+			<%} %>
+		</tbody>
+		
+		<tfoot>
+			<tr>
+				<td colspan="8" align="right">
+					<a href="bath.jsp">
+						<input type="button" value="견적받으러가깅">
+					</a>
+				</td>
+			</tr>
+		</tfoot>
+	</table>
+	
+
+</body>
+</html>
---
+++ ssemi/src/home/servlet/estimate/EstimateSaveServlet.java
@@ -0,0 +1,57 @@
+package home.servlet.estimate;
+
+import java.io.IOException;
+import java.sql.PreparedStatement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import home.beans.dao.EstimateDao;
+import home.beans.dto.EstimateDto;
+import home.beans.dto.MemberDto;
+
+@WebServlet(urlPatterns = "/estimate/saveesti.do")
+public class EstimateSaveServlet extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+
+			
+			  req.setCharacterEncoding("UTF-8");
+			
+			MemberDto mdto = (MemberDto) req.getSession().getAttribute("userinfo");
+			String member_id = mdto.getMember_id();//작성자 추출
+			
+			EstimateDto edto = new EstimateDto();
+			edto.setBath_price(Integer.parseInt(req.getParameter("bath_price")));
+			edto.setBath_cnt(req.getParameter("bath_cnt"));
+			edto.setBath_cntt(req.getParameter("bath_cntt"));
+			edto.setBath_tub(req.getParameter("bath_tub"));
+			edto.setBath_tile(req.getParameter("bath_tile"));
+			edto.setBath_option(req.getParameter("bath_option"));
+			edto.setBath_date(req.getParameter("bath_date"));
+			edto.setBath_member(member_id);
+		
+			EstimateDao edao = new EstimateDao();
+			edao.save(edto);
+			resp.sendRedirect("bath.jsp");
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			resp.sendError(500);//--> 미리 등록된 에러 페이지 500번으로 연동
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
